21e2fde76b1dd44b0f1b5080e6b2e576eb947a42
DainikGupta <54498836+DainikGupta@users.noreply.github.com>
2019-10-24 01:27:06 +0530

    Add files via upload

---
+++ Q1.s
@@ -0,0 +1,25 @@
+    PRESERVE8
+	THUMB
+	AREA    |.text|,CODE,READONLY
+    EXPORT __main
+    ENTRY
+__main    FUNCTION
+		MOV R0, #0
+		MOV R1, #1
+		MOV R2, #10	;COUNT
+		MOV R3, #0
+		MOV R4, #0x20000000
+				
+loop1	CMP R2, #0	;
+		BNE LOOP2
+		BEQ stop
+		
+LOOP2	SUB R2, R2, #1
+		ADD R3, R0, R1
+		MOV R0, R1
+		MOV R1, R3
+		B loop1
+	
+stop    B stop
+		ENDFUNC
+		END
