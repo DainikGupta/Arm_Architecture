6280266dd0f0c99dcb418116ca4182f5f36b8c2b
DainikGupta <54498836+DainikGupta@users.noreply.github.com>
2019-11-08 12:54:55 +0530

    Create Q5_MT2018507.s

---
+++ Q5_MT2018507.s
@@ -0,0 +1,4 @@
+We can give aninput to a hardware board through GPIO pins.
+It will generate an interrupt.
+This will be handled by an Interrupt Handler.
+The processor will tak the input and then it will again return to the main program.
